d3a27be0dc5262fac5e487baa109081dc6ba985c
Misha Magnusson <misha.magnusson04@gmail.com>
2025-01-23 23:41:57 +0100

    fixed

---
+++ get-file.sh
@@ -0,0 +1,12 @@
+TIME=$(date "+%Y-%m-%d %H:%M:%S")
+
+
+SOURCE="/home/misha/Downloads/katten gustavs äventyr.html"
+DESTINATION="$(pwd)/index.html"
+OLD="$(pwd)/old/index"$TIME".html"
+
+#Get old index.html out the way
+mv "$DESTINATION" "$OLD"
+
+#bring in the new index.html
+mv "$SOURCE" "$DESTINATION"
